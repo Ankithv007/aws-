e3e53a0ec1c885b4b830c9a0e216467466803bee
Ankith b v <ankithbv007@gmail.com>
2024-08-29 00:06:44 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -e
 
+# Print a message to indicate script execution
+echo "Stopping container..."
+
 # Stop the running container (if any)
-echo "Hi"
+container_id=$(docker ps -q --filter "ancestor=ankithbv007/simple-python-flask-app")
+
+if [ -n "$container_id" ]; then
+  echo "Stopping and removing container ID: $container_id"
+  docker rm -f $container_id
+else
+  echo "No running container found with the image 'ankithbv007/simple-python-flask-app'."
+fi
