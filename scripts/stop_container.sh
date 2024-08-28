ad39908b2428fe4c81db3ec344bd39a34447705f
Ankith b v <ankithbv007@gmail.com>
2024-08-28 23:43:23 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,11 +2,4 @@
 set -e
 
 # Stop the running container (if any)
-container_id=$(docker ps -q --filter "name=my-container")
-
-if [ -n "$container_id" ]; then
-  echo "Stopping and removing container ID: $container_id"
-  docker rm -f $container_id
-else
-  echo "No running container found with the name 'my-container'."
-fi
+echo "Hi"
